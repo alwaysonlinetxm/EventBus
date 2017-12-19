913a0f2e6959ae0e61c507bfefa9466fd448c04c
alwaysonlinetxm <alwaysonlinetxm@163.com>
2017-12-19 17:09:51 +0800

    update function name

--- src/index.js
+++ src/index.js
@@ -1,66 +1,66 @@
 class EventBus {
   constructor() {
-    this.eventCenter = {};
+    this._eventCenter = {};
   }
 
   // register an event
   register(type) {
-    const { eventCenter, throwError } = this;
+    const { _eventCenter, _throwError } = this;
 
-    if (!eventCenter[type]) {
-      eventCenter[type] = new Set();
+    if (!_eventCenter[type]) {
+      _eventCenter[type] = new Set();
     } else {
       // illegal means the name may be same with a extended property
-      throwError(`the event "${type}" has been used or is illegal!`);
+      _throwError(`the event "${type}" has been used or is illegal!`);
     }
   }
 
   // remove a registered event
   remove(type) {
     // the 'delete' will only worked on own property
-    delete this.eventCenter[type];
+    delete this._eventCenter[type];
   }
 
   // subscribe an event
   subscribe(type, cb) {
-    if (this.checkType(type)) {
-      this.eventCenter[type].add(cb);
+    if (this._checkType(type)) {
+      this._eventCenter[type].add(cb);
     }
   }
 
   // cancel subscribing an event, the 'cb' should has been added by 'subscribe'
   cancel(type, cb) {
-    if (this.checkType(type)) {
-      this.eventCenter[type].delete(cb);
+    if (this._checkType(type)) {
+      this._eventCenter[type].delete(cb);
     }
   }
 
   // publish en event
   publish(type, data = {}) {
-    if (this.checkType(type)) {
+    if (this._checkType(type)) {
       try {
-        for (const cb of this.eventCenter[type].values()) {
+        for (const cb of this._eventCenter[type].values()) {
           if (typeof cb === 'function') {
             cb(data);
           }
         }
       } catch (e) {
         console.error(e);
-        this.throwError('error in callback!')
+        this._throwError('error in callback!')
       }
     }
   }
   // check the 'type' whether exists
-  checkType(type) {
-    if (this.eventCenter.hasOwnProperty(type)) {
+  _checkType(type) {
+    if (this._eventCenter.hasOwnProperty(type)) {
       return true;
     } else {
-      this.throwError(`the event "${type}" does not exist! ----`);
+      this._throwError(`the event "${type}" does not exist! ----`);
       return false;
     }
   }
 
-  throwError(msg) {
+  _throwError(msg) {
     console.error(`---- ${msg} ----`);
     throw msg;
   }
